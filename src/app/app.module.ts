6935172a1c4893f67fc93d8c45524bca9a8582d8
sivarajeshcashapona <sivarajesh.gudala@cashapona.com>
2021-05-18 17:45:36 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ArrayMethodsComponent } from './array-methods/array-methods.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ArrayMethodsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/array-methods/array-methods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-array-methods',
+  templateUrl: './array-methods.component.html',
+  styleUrls: ['./array-methods.component.css']
+})
+export class ArrayMethodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
