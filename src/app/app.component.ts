3f32cb1fd2dc42212425f4393c4dc673eec3577b
sivarajeshcashapona <sivarajesh.gudala@cashapona.com>
2021-05-18 20:39:55 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,59 +14,59 @@ export class AppComponent implements OnInit {
     // rxjs Operators:
     // 1. of
 
-    const personalInfo: any = {
-      name: 'Rajesh',
-      Contact: '8328348050',
-      place: 'visakhapatnam'
-    };
+    // const personalInfo: any = {
+    //   name: 'Rajesh',
+    //   Contact: '8328348050',
+    //   place: 'visakhapatnam'
+    // };
 
-    const personObj: Observable<String> = of(personalInfo);
-    personObj.subscribe(data => {
-      console.log(data);
-    });
+    // const personObj: Observable<String> = of(personalInfo);
+    // personObj.subscribe(data => {
+    //   console.log(data);
+    // });
 
     // 2. From
 
-    const promise: Promise<String> = Promise.resolve(personalInfo);
-    const personPromise: Observable<any> = from(promise);
-    personPromise.subscribe(data => {
-      console.log(data);
-    });
+    // const promise: Promise<String> = Promise.resolve(personalInfo);
+    // const personPromise: Observable<any> = from(promise);
+    // personPromise.subscribe(data => {
+    //   console.log(data);
+    // });
 
-    const array = [1, 2, 3, 'Chris Evans', 'deadpool'];
+    // const array = [1, 2, 3, 'Chris Evans', 'deadpool'];
 
-    const rxjsOf: Observable<any> = of(array);
-    rxjsOf.subscribe(arr => {
-      console.log(arr);
-    });
+    // const rxjsOf: Observable<any> = of(array);
+    // rxjsOf.subscribe(arr => {
+    //   console.log(arr);
+    // });
 
-    const rxjsFrom: Observable<any> = from(array);
-    rxjsFrom.subscribe(arr => {
-      console.log(arr);
-    });
+    // const rxjsFrom: Observable<any> = from(array);
+    // rxjsFrom.subscribe(arr => {
+    //   console.log(arr);
+    // });
 
     // 3. Pipe, Map
 
-    const dataArray = ['Rajesh', 'mahesh', 'katherine', 'angular', 'React'];
+    // const dataArray = ['Rajesh', 'mahesh', 'katherine', 'angular', 'React'];
 
-    dataArray.forEach((item, i, arr) => {
-      const newArray: any = of(arr[i]);
+    // dataArray.forEach((item, i, arr) => {
+    //   const newArray: any = of(arr[i]);
 
-      newArray
-        .pipe(map((data: any) => data.toUpperCase()))
-        .subscribe(data => console.log(data));
-    });
+    //   newArray
+    //     .pipe(map((data: any) => data.toUpperCase()))
+    //     .subscribe(data => console.log(data));
+    // });
 
-    const source: any = of(1, 2, 3, 4, 5);
-    // transparently log values from source with 'tap'
-    const example = source.pipe(
-      tap(val => console.log(`BEFORE MAP: ${val}`)),
-      map((val: any) => val + 10),
-      tap(val => console.log(`AFTER MAP: ${val}`))
-    );
+    // const source: any = of(1, 2, 3, 4, 5);
+    // // transparently log values from source with 'tap'
+    // const example = source.pipe(
+    //   tap(val => console.log(`BEFORE MAP: ${val}`)),
+    //   map((val: any) => val + 10),
+    //   tap(val => console.log(`AFTER MAP: ${val}`))
+    // );
 
     //'tap' does not transform values
     //output: 11...12...13...14...15
-    const subscribe = example.subscribe(val => console.log(val));
+    // const subscribe = example.subscribe(val => console.log(val));
   }
 }
--- src/app/array-methods/array-methods.component.ts
+++ src/app/array-methods/array-methods.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { elementAt } from 'rxjs/dist/types/operators';
 
 @Component({
   selector: 'app-array-methods',
@@ -6,10 +7,134 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./array-methods.component.css']
 })
 export class ArrayMethodsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    // 1. Slice ----> it doesn't mutates the original array it creates copy of another array
+    const arr = ['1', '2', '3', '4', '5'];
+    console.log('1.slice');
+    console.log(arr.slice());
+    console.log(arr.slice(2, 4));
+    console.log(arr.slice(-2));
+    console.log([...arr]); // spread operator same as slice
+    // - sign indicates last part of array
+    console.log('2.splice');
 
+    // 2. Splice removes the part of original array.
+    // it mutates the original array
+    console.log(arr.splice(-1), arr);
+
+    // 3. Reverse: simply reverse the array elements
+    // it mutates the original array
+    console.log('3.reverse');
+    console.log(arr.reverse(), arr);
+
+    //Concat
+    console.log('Concat');
+    const arr1 = ['3', '5', '5', '8', '9'];
+    const arr2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(arr1.concat(arr2));
+
+    //split and join
+    console.log('Split and Join');
+    arr2.forEach((item, i, arr) => {
+      const newArray = arr[i];
+      console.log(newArray.split(',').join('-'));
+    });
+
+    // Looping throgh Arrays
+    // Unlike forEach(), you can use break, continue, and return
+    // If you want to loop over an object’s properties, either use for-in
+    const mixedArray: any = [50, 28, 'rajesh', 98, 'hello', 'Hii'];
+    console.log('****For Of*****');
+    for (const [i, value] of mixedArray.entries()) {
+      if (typeof value === typeof '') {
+        console.log(
+          `${i + 1}. ${value} is of type String with length ${value.length}`
+        );
+      } else {
+        console.log(
+          `${i + 1}. ${value} is of type Number with length ${value.length}`
+        );
+      }
+    }
+    console.log('*********For Each*****');
+    mixedArray.forEach((val, index, arr) => {
+      if (typeof val === typeof '') {
+        console.log(
+          `${index + 1}. ${val} is of type String with length ${val.length}`
+        );
+      } else {
+        console.log(
+          `${index + 1}. ${val} is of type Number with length ${val.length}`
+        );
+      }
+    });
+
+    console.log('--------------------');
+    // For-each with Map and Sets
+    const currencies = new Map([
+      ['USD', 'Unites States Dollar'],
+      ['INR', 'Indian Rupee'],
+      ['EUR', 'Euro']
+    ]);
+
+    currencies.forEach((val, key, arr) => {
+      console.log(`${key}:${val}`);
+    });
+    const currenciesSets = new Set([
+      'USD',
+      'Unites States Dollar',
+      'INR',
+      'Indian Rupee',
+      'EUR',
+      'Euro'
+    ]);
+
+    currenciesSets.forEach((val, _, arr) => {
+      console.log(`${val}:${val}`);
+    });
+
+    // data tranformations with map, filter and reduce
+
+    // Map--> mutates the original array and stores into new array
+
+    const arrayModel = [500, 1000, 6000, 8000, 10000];
+    const mappedArray = arrayModel.map(val => (val * 2) / 2);
+    console.log(mappedArray);
+
+    // map can also used to loop through array
+    arrayModel.map((val, i, arr) => {
+      console.log(
+        `${i + 1}. the array element is ${val} of length ${
+          val.toString().length
+        }`
+      );
+    });
+
+    //Filter ---> it returns new array containing array elements that passes a specified condition
+
+    const depositsArray = [500,2000, 5000,6000,1000,200,7000]
+    const greaterthan2K = depositsArray.filter((val)=> {
+      return val > 2000
+    });
+    console.log(`The deposits greater than 2000 are : ${greaterthan2K}`);
+
+    const lessthan2K = depositsArray.filter((val)=> {
+      return val < 2000
+    })
+        console.log(`the deposits greater than 2000 are : ${lessthan2K}`);
+
+
+    // reduce adds all the elements to a single value
+    // acc ---> accumulator
+    // cur --> current element
+    // i ---> index
+    // arr ---> array
+    const totalAmount= depositsArray.reduce((acc,cur,i,arr)=> {
+      console.log(`Iteration ${i}: ${acc}`);
+      return acc+cur;
+    },100);
+    console.log(`The total deposit amount is:  ${totalAmount}`)
+  }
 }
